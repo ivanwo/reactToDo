83786d8383ed8e2207f22e0967e60f8d1407b7db
ivan <ivanwolyniec@gmail.com>
2020-01-29 15:19:33 -0500

    pages build

---
+++ docs/precache-manifest.9cbd299050899f729825cfc64f6a3e48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ea37d1b9fa2781925adcd718b335cb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b3853d5dfd7cd04c190",
+    "url": "/static/css/main.267ead25.chunk.css"
+  },
+  {
+    "revision": "e0697a57b84fe137f44a",
+    "url": "/static/js/2.504d5b32.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.504d5b32.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0b3853d5dfd7cd04c190",
+    "url": "/static/js/main.27454887.chunk.js"
+  },
+  {
+    "revision": "47416c890b8c52cb5b9b",
+    "url": "/static/js/runtime-main.6334459c.js"
+  }
+]);
